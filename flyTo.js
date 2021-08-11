f652f7f70ea32baf532e8ee518a00628377829c7
Felix Palmer <felixpalmer@gmail.com>
2021-08-11 17:19:54 +0200

    Handle heading discontinuity in tween

--- flyTo.js
+++ flyTo.js
@@ -2,10 +2,18 @@ import {Easing, Tween} from '@tweenjs/tween.js';
 
 export default function flyTo(map, position) {
   const center = map.getCenter();
+  let heading = map.getHeading();
+  if (position.heading !== undefined) {
+    if (position.heading - heading > 180) {
+      heading += 360;
+    } else if (position.heading - heading < -180) {
+      heading -= 360;
+    }
+  }
   const tween = new Tween({
     lat: center.lat(),
     lng: center.lng(),
-    heading: map.getHeading(),
+    heading,
     tilt: map.getTilt(),
     zoom: map.getZoom()
   }).easing(Easing.Quadratic.InOut);
