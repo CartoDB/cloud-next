3c1072f8b92ed867eaf9fb24f07613a091914aaa
Laurent Dietrich <laurent@solublestudio.com>
2021-09-01 19:13:02 +0200

    Add svg support and cover WIP

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ const CONFIG = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader']
       }
     ]
   },
