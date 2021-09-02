95cf6a276789567d9cfba61428fd2e192148b2ef
Felix Palmer <felixpalmer@gmail.com>
2021-09-02 16:31:33 +0200

    Handle cloning on deferred load layer better

--- slides/deferredLoadLayer.js
+++ slides/deferredLoadLayer.js
@@ -3,22 +3,28 @@ import {CompositeLayer} from '@deck.gl/core';
 // Utility to create a layer that only is initialized
 // (and thus data loaded) once visible
 export default function DeferredLoadLayer(createLayer) {
+  let subLayer = null;
   return class DeferredLoadLayer extends CompositeLayer {
-    renderLayers() {
-      const {visible} = this.props;
-      const subLayers = this.getSubLayers();
-      if (subLayers.length !== 0) {
-        // Return cached sublayers to avoid re-fetching data
-        return subLayers.map(l => l.clone({visible}));
+    clone(props) {
+      if (subLayer) {
+        subLayer = subLayer.clone(props);
       }
+      return super.clone(props);
+    }
+
+    renderLayers() {
+      const {visible, pointRadiusScale} = this.props;
 
       // Only create sublayer lazily if visible
       if (!visible) {
         return [];
       }
-      const subLayer = createLayer().clone({
-        id: `deferred-${this.id}`
-      });
+      if (!subLayer) {
+        console.log('creating', this.id);
+        subLayer = createLayer().clone({
+          id: `deferred-${this.id}`
+        });
+      }
       return [subLayer];
     }
   };
