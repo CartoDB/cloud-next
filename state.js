70773fc442093f649d8097875b0474dff3a051ad
Felix Palmer <felixpalmer@gmail.com>
2021-09-02 12:45:45 +0200

    Tidy

--- state.js
+++ state.js
@@ -1,10 +1,4 @@
 import React, {useState, createContext, useContext, useEffect} from 'react';
-import {
-  getTexasBoundarySimplifiedData,
-  getWKTData,
-  getPopulationData,
-  getTexasTripData
-} from './datasource';
 import {createOverlay} from './overlay';
 import {loadScript} from './utils';
 
