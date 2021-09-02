da8e7d5dbf5c0d3254cf3857b7e7513098135a8f
Felix Palmer <felixpalmer@gmail.com>
2021-09-02 15:45:50 +0200

    Defer data loading with DefferedLoadMixin

--- slides/population.js
+++ slides/population.js
@@ -1,12 +1,33 @@
+import {CompositeLayer} from '@deck.gl/core';
 import {H3HexagonLayer} from '@deck.gl/geo-layers';
 import {getPopulationData} from '../datasource';
 
-export const PopulationLayer = new H3HexagonLayer({
-  id: 'population-heatmap',
-  data: getPopulationData(),
-  extruded: true,
-  elevationScale: 2,
-  getHexagon: d => d.h3,
-  getElevation: d => d.pop,
-  getFillColor: d => [255, (1 - d.pop / 30000) * 255, 0]
+function DeferredLoadMixin(createLayer) {
+  return class DeferredLoadLayer extends CompositeLayer {
+    renderLayers() {
+      if (!this.props.visible) {
+        return [];
+      }
+
+      const subLayer = createLayer().clone({
+        id: `deffered-${this.id}`
+      });
+      return [subLayer];
+    }
+  };
+}
+
+const Layer = DeferredLoadMixin(() => {
+  return new H3HexagonLayer({
+    data: getPopulationData(),
+    extruded: true,
+    elevationScale: 2,
+    getHexagon: d => d.h3,
+    getElevation: d => d.pop,
+    getFillColor: d => [255, (1 - d.pop / 30000) * 255, 0]
+  });
+});
+
+export const PopulationLayer = new Layer({
+  id: 'population-heatmap'
 });
