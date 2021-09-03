f0423d0e41c51a3f88fc9a49f75d07cbadc56568
Felix Palmer <felixpalmer@gmail.com>
2021-09-03 17:27:27 +0200

    Add views

--- state.js
+++ state.js
@@ -1,4 +1,5 @@
 import React, {useState, createContext, useContext, useEffect} from 'react';
+import slides from './slides';
 import {createOverlay} from './overlay';
 import {loadScript} from './utils';
 
@@ -7,17 +8,6 @@ import flyTo from './flyTo';
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
 const GOOGLE_MAP_ID = '84591267f7b3a201';
 const GOOGLE_MAPS_API_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=beta&map_ids=${GOOGLE_MAP_ID}`;
-const slides = [
-  /* 0 */ {layers: ['roads', 'texas-boundary'], view: {}},
-  /* 1 */ {layers: ['population-heatmap', 'texas-boundary' /*'texas-counties'*/], view: {}},
-  /* 2 */ {layers: ['power-lines', 'texas-boundary'], view: {}},
-  /* 3 */ {layers: ['energy-sources', 'energy-sources-background', 'texas-boundary'], view: {}},
-  /* 4 */ {layers: ['traffic-flow'], view: {}},
-  /* 5 */ {layers: ['truck-trips', 'texas-boundary'], view: {}},
-  /* 6 */ {layers: ['scenegraph-layer'], view: {}},
-  /* 7 */ {layers: [], view: {}},
-  /* 8 */ {layers: ['temperature', 'texas-boundary'], view: {}}
-];
 
 const initAppState = {
   currentSlide: null
@@ -53,7 +43,9 @@ export const AppStateStore = ({children}) => {
   useEffect(
     () => {
       if (currentSlide !== null && overlay?.visibleLayers) {
-        overlay.visibleLayers = slides[currentSlide].layers;
+        const {layers, view} = slides[currentSlide];
+        overlay.visibleLayers = layers;
+        flyTo(map, view);
       }
     },
     [currentSlide]
@@ -111,6 +103,6 @@ window.print = () => {
   const heading = map.getHeading();
   const tilt = map.getTilt();
   const zoom = map.getZoom();
-  const config = {center: {lat, lng}, heading, tilt, zoom};
+  const config = {lat, lng, heading, tilt, zoom};
   console.log(JSON.stringify(config));
 };
