aa0bdf33d1da137a47ee1bf8ddca630d1f53a7e8
Felix Palmer <felixpalmer@gmail.com>
2021-09-07 14:49:56 +0200

    Remove old flyTo

--- flyTo.js
+++ flyTo.js
@@ -47,35 +47,3 @@ export default function flyTo(map, position) {
     })
     .start();
 }
-
-function flyToOld(map, position) {
-  const center = map.getCenter();
-  let heading = map.getHeading();
-  if (position.heading !== undefined) {
-    if (position.heading - heading > 180) {
-      heading += 360;
-    } else if (position.heading - heading < -180) {
-      heading -= 360;
-    }
-  }
-
-  const zoom = map.getZoom();
-  let duration = 1000;
-  if (position.zoom !== undefined) {
-    duration += 100 * Math.abs(position.zoom - zoom);
-  }
-  const tween = new Tween({
-    lat: center.lat(),
-    lng: center.lng(),
-    heading,
-    tilt: map.getTilt(),
-    zoom
-  })
-    .easing(Easing.Quadratic.InOut)
-    .duration(duration)
-    .to(position)
-    .onUpdate(({lat, lng, heading, tilt, zoom}) => {
-      map.moveCamera({center: {lat, lng}, heading, tilt, zoom});
-    })
-    .start();
-}
