0b2d5e08276c75eb3884e196762456c6b75425c8
Felix Palmer <felixpalmer@gmail.com>
2021-09-07 16:35:15 +0200

    Try a more consistent color scheme for flowmap

--- layers/trafficFlow.js
+++ layers/trafficFlow.js
@@ -1,5 +1,6 @@
-import DeferredLoadLayer from './deferredLoadLayer';
 import FlowmapLayer from './flowmap';
+import DeferredLoadLayer from './deferredLoadLayer';
+import Blending from './blending';
 import {flows, locations} from '../data/od_texas';
 
 const flowmapStyle = {
@@ -7,34 +8,18 @@ const flowmapStyle = {
   colors: {
     flows: {
       scheme: [
-        'rgb(255, 255, 176)',
-        'rgb(253, 247, 140)',
-        'rgb(255, 231, 107)',
-        'rgb(255, 214, 86)',
-        'rgb(255, 195, 76)',
-        'rgb(255, 177, 74)',
-        'rgb(255, 160, 78)',
-        'rgb(255, 144, 88)',
-        'rgb(255, 130, 101)',
-        'rgb(247, 117, 114)',
-        'rgb(230, 105, 125)',
-        'rgb(210, 95, 135)',
-        'rgb(187, 85, 141)',
-        'rgb(165, 74, 144)',
-        'rgb(141, 63, 143)',
-        'rgb(118, 52, 138)',
-        'rgb(93, 40, 127)',
-        'rgb(64, 31, 105)',
-        'rgb(37, 24, 71)',
-        'rgb(17, 16, 34)',
-        'rgb(0, 0, 4)'
+        'rgb(253, 230, 219)',
+        'rgb(248, 163, 171)',
+        'rgb(241, 77, 143)',
+        'rgb(182, 0, 119)',
+        'rgb(101, 0, 100)'
       ]
     },
     locationCircles: {outgoing: '#fff'},
     outlineColor: 'rgba(255, 255, 255, 0.5)'
   },
-  locationTotalsExtent: [124, 1568915],
-  maxFlowThickness: 18,
+  locationTotalsExtent: [12004, 1568915],
+  maxFlowThickness: 16,
   maxLocationCircleSize: 15,
   showOnlyTopFlows: 5000,
   showTotals: true,
